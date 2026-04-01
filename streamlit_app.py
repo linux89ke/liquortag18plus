44ba0121437566263cb4fe28bf2f24d3ad5e15cc
linux89ke <161024953+linux89ke@users.noreply.github.com>
2026-04-01 11:55:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+streamlit>=1.56.0
+Pillow
+requests
+numpy
+pandas
+openpyxl
+beautifulsoup4
+selenium
+webdriver-manager
